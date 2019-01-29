de16e7d29cca87b2842903a3cfcf1050af92137d
cconner <cconner@cloudera.com>
2019-01-29 13:30:25 -0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,11 @@ set -x
 
 nohup sleep 200000
 
-#nohup /usr/sbin/sshd -D &
+nohup /usr/sbin/sshd -D &
 
-#chown -R vagrant: ${VAGRANT_DATA}
-#su - vagrant
+chown -R vagrant: ${VAGRANT_DATA}
+su - vagrant
+
+env
 #cd ${VAGRANT_PATH} && ${BUNDLE} binstubs bundler --force
 
