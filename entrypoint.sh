8d10c988709e083c6155565ddd6677630912ec9e
cconner <cconner@cloudera.com>
2019-01-29 13:05:18 -0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -x
 
+nohup /usr/sbin/sshd -D &
+
 chown -R vagrant: ${VAGRANT_DATA}
 su - vagrant
 cd ${VAGRANT_PATH} && ${BUNDLE} binstubs bundler --force
-exit
-/usr/sbin/sshd -D
+
